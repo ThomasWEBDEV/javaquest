2185b620d0050e39b244df5c40311bbab47aee22
Feret Thomas <thomas.feret@hotmail.fr>
2026-03-13 15:04:40 +0100

    backend: classe principale spring boot

---
+++ backend/src/main/java/com/javaquest/JavaQuestApplication.java
@@ -0,0 +1,16 @@
+package com.javaquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Point d'entree principal de l'application JavaQuest.
+ * Plateforme d'apprentissage Java interactive.
+ */
+@SpringBootApplication
+public class JavaQuestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaQuestApplication.class, args);
+    }
+}
