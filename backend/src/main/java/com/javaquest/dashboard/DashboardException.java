56cd07688a0213937c98df7079f93e2fd20803ed
Feret Thomas <thomas.feret@hotmail.fr>
2026-03-25 14:11:34 +0100

    backend: exception dashboard

---
+++ backend/src/main/java/com/javaquest/dashboard/DashboardException.java
@@ -0,0 +1,11 @@
+package com.javaquest.dashboard;
+
+/**
+ * Exception levee lors d'erreurs liees au dashboard.
+ */
+public class DashboardException extends RuntimeException {
+
+    public DashboardException(String message) {
+        super(message);
+    }
+}
