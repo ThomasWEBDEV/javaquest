6cee13ec9bae2d9a1acf1e492d83fbdbe9d4b895
Feret Thomas <thomas.feret@hotmail.fr>
2026-03-31 15:18:59 +0200

    frontend: configuration app et main

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,20 @@
+<template>
+  <router-view />
+</template>
+
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted } from 'vue'
+import { useAuthStore } from '@/stores/auth'
+
+const authStore = useAuthStore()
+
+onMounted(() => {
+  if (authStore.token) {
+    authStore.fetchUser()
+  }
+})
 </script>
 
-<template>
-  <HelloWorld />
-</template>
+<style>
+@import 'tailwindcss';
+</style>
